c92e58f5e8680d7e71868ea28f96aebf42fa400a
karri <husnoraxamitova@gmail.com>
2025-05-10 09:33:59 +0500

    fronted

--- keyboards.py
+++ keyboards.py
@@ -1,7 +1,7 @@
 from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton, WebAppInfo
 from aiohttp import web
 
-web_app = WebAppInfo(url="https://google.com")
+web_app = WebAppInfo(url="https://chatgpt.com/")
 web_app_btn=InlineKeyboardMarkup(
     inline_keyboard=[
         [InlineKeyboardButton(text="Web App", web_app=web_app)],
